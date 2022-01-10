f884243cb0d52414d08321a35ffaf5affa24b88f
Pratishtha Verma <pratishthaverma@TTNs-MacBook-Pro.local>
2022-01-10 13:52:00 +0530

    Created Node Group Module

---
+++ modules/node_group/lt.tf
@@ -0,0 +1,26 @@
+resource "aws_launch_template" "ng_eks_launch_template" {
+  name = "ng_eks_launch_template"
+
+  vpc_security_group_ids  = ["sg-030888ba8b196acc4", "sg-024ac5730477138b4"]
+  disable_api_termination = false
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 20
+      volume_type = "gp2"
+    }
+  }
+
+  image_id = "ami-002539dd2c532d0a5"
+  #instance_type = "t3.medium"
+  user_data = base64encode(templatefile("${path.module}/user-data-apache.sh", {}))
+  #user_data = "${file("user-data-apache.sh")}"
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "EKS-MANAGED-NODE"
+    }
+  }
+}
---
+++ modules/node_group/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_eks_node_group" "terraform_node_group" {
+  cluster_name    = "testing-cluster"
+  node_group_name = "testing-node-group"
+  node_role_arn   = aws_iam_role.eks-iam-ng.arn
+  subnet_ids      = ["subnet-055eb434f841c1ec7", "subnet-082d1b94bacae6504", "subnet-05dec220d2885f6dd"]
+  instance_types  = ["t3.medium", "t2.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 4
+    min_size     = 1
+  }
+
+
+  launch_template {
+    name    = aws_launch_template.ng_eks_launch_template.name
+    version = aws_launch_template.ng_eks_launch_template.latest_version
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [aws_iam_role_policy_attachment.eks-iam-ng-AmazonEKSWorkerNodePolicy, aws_iam_role_policy_attachment.eks-iam-ng-AmazonEKS_CNI_Policy, aws_iam_role_policy_attachment.eks-iam-ng-AmazonEC2ContainerRegistryReadOnly, aws_iam_role_policy_attachment.eks-iam-ng-AmazonSSMManagedInstanceCore,
+  ]
+}
+
+resource "aws_iam_role" "eks-iam-ng" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-ng-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-iam-ng.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-ng-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-iam-ng.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-ng-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-ng.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-iam-ng-AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.eks-iam-ng.name
+}
