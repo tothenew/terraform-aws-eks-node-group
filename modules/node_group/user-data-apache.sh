ea3d6362047ca18712ddd14ccf238aaeee2134dc
Pratishtha Verma <pratishthaverma@TTNs-MacBook-Pro.local>
2022-01-28 16:39:23 +0530

    Updated File Structure

--- modules/node_group/user-data-apache.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-/etc/eks/bootstrap.sh 'testing-cluster'
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "The page was created by the user data" | sudo tee /var/www/html/index.html
