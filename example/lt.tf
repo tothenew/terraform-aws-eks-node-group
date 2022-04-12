c941e5455e8bddca61905614255d6ba702320205
Pratishtha Verma <pratishthaverma@ttnpl-4654.local>
2022-04-12 11:50:05 +0530

    Added Example

---
+++ example/lt.tf
@@ -0,0 +1,26 @@
+resource "aws_launch_template" "ng_eks_launch_template" {
+  name = var.lt_name
+
+  vpc_security_group_ids  = var.vpc_security_group_ids
+  disable_api_termination = var.disable_api_termination
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = var.volume_size
+      volume_type = var.volume_type
+    }
+  }
+
+  image_id  = var.image_id
+  user_data = base64encode(templatefile("${path.module}/user-data-apache.sh", {}))
+  #user_data = "${file("user-data-apache.sh")}"
+  key_name = var.key_name
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "var.Name"
+    }
+  }
+}
---
+++ example/user-data-apache.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+/etc/eks/bootstrap.sh 'testing-cluster'
